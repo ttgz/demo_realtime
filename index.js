0dce9db19a2af3ec3881d9a155207a1df11a1afc
DOng <khanhdong1702pro@gmail.com>
2025-05-24 21:00:04 +0700

    update

--- index.js
+++ index.js
@@ -21,29 +21,37 @@ app.get('/', (req, res) => {
 // Lắng nghe kết nối từ client
 io.on('connection', (socket) => {
     console.log('Client đã kết nối');
-    setInterval(() => {
-        // Giả lập dữ liệu từ thiết bị
-        const data = {
-            "sali": Math.random().toFixed(2) * 100, // Giả lập dữ liệu nồng độ muối
-            "temp": (Math.random() * 30 + 20).toFixed(2), // Giả lập nhiệt độ
-            "do": (Math.random() * 10).toFixed(2), // Giả lập nồng độ oxy hòa tan
-            "ph": (Math.random() * 14).toFixed(2) // Giả lập độ pH
-        };
-        io.emit('device-data', data);    // Gửi dữ liệu giả lập mỗi 1 giây
-    }, 1000); // 1000ms = 1 giây // Gửi dữ liệu giả lập mỗi 1 giây
-  
+
+
+
+
+
 });
+function getRandomFloat(min, max) {
+    return Math.random() * (max - min) + min;
+}
+// Giả lập dữ liệu từ thiết bị
+setInterval(() => {
+    const data = {
+        "sali": getRandomFloat(0, 4).toFixed(2), // Giả lập dữ liệu nồng độ muối
+        "temp": getRandomFloat(20, 50).toFixed(2), // Giả lập nhiệt độ
+        "do": getRandomFloat(0, 10).toFixed(2), // Giả lập nồng độ oxy hòa tan
+        "ph": getRandomFloat(0, 14).toFixed(2) // Giả lập độ pH
+    };
+    console.log('Dữ liệu giả lập:', data);
+    io.emit('device-data', data);    // Gửi dữ liệu giả lập mỗi 1 giây
+}, 1000); // 1000ms = 1 giây // Gửi dữ liệu giả lập mỗi 1 giây
 
 app.post('/api/data', (req, res) => {
     const data = req.body;
     console.log('Dữ liệu từ thiết bị:', data);
-    if(!data.sali)
+    if (!data.sali)
         data.sali = '...';
-    if(!data.temp)
+    if (!data.temp)
         data.temp = '...';
-    if(!data.do)
+    if (!data.do)
         data.do = '...';
-    if(!data.ph)
+    if (!data.ph)
         data.ph = '...';
     // Gửi realtime tới Web
     io.emit('device-data', data);
